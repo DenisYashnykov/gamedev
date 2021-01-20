f6e2412cdcd39cec60a3510fc9b2ea3697833224
Denys Yashnykov <revillz.nice@gmail.com>
2021-01-20 22:09:34 +0200

    lab1

---
+++ lab1/Assets/Scripts/RotatingObject.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatingObject : MonoBehaviour
+{
+    [SerializeField] private Transform _transformObj;
+    [SerializeField] private float _rotationSpeed;
+
+    private void Update()
+    {
+        _transformObj.rotation *= Quaternion.Euler(Vector3.one * _rotationSpeed * Time.deltaTime);
+    }
+}
---
+++ lab1/Assets/Scripts/ScaleObj.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScaleObj : MonoBehaviour
+{
+
+    [SerializeField] private Transform _trasform;
+    [SerializeField] private float min=1.0f, max=5.0f;
+    [SerializeField] private float _scaleSpeed;
+    [SerializeField] private bool _increase=true;
+    void Update()
+    {
+        if (_increase)
+        {
+            _trasform.localScale += Vector3.one*_scaleSpeed* Time.deltaTime;
+            if (_trasform.localScale.x >= max) 
+            { 
+                _increase = false; 
+            }
+        }else
+            
+            {
+               _trasform.localScale -= Vector3.one * _scaleSpeed* Time.deltaTime; 
+               if(_trasform.localScale.x <= min)
+                {
+                _increase = true;
+                }
+                
+            }
+
+    }
+}
