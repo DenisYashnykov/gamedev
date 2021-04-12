01041cf62f594c913fe78c1931104d0fe0d43220
Denys Yashnykov <revillz.nice@gmail.com>
2021-04-12 20:11:37 +0300

    lr5

---
+++ lr5/Assets/Scripts/Enemy.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] private float _resTime;
+    [SerializeField] private PlayerController _player;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Bullet")
+        {
+            _player.KillEnemy();
+
+            gameObject.SetActive(false);
+            DOVirtual.DelayedCall(_resTime, () =>
+            {
+                gameObject.SetActive(true);
+            });
+        }
+    }
+}
