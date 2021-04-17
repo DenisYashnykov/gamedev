f0538e27a0caf3a17d91fe52a926164b3a3a26b7
Denys Yashnykov <revillz.nice@gmail.com>
2021-04-17 14:16:04 +0300

    newLR5

---
+++ newLR5/Assets/Scripts/Playfab.cs
@@ -0,0 +1,57 @@
+﻿using PlayFab;
+using PlayFab.ClientModels;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PlayFab.PfEditor.Json;
+
+namespace Game
+{
+    public class Playfab : MonoBehaviour
+    {
+        private void Start()
+        {
+            LoginToPlayFab();
+        }
+
+        private void LoginToPlayFab()
+        {
+
+            var request = new LoginWithCustomIDRequest { CustomId = "EDITOR_DEBUG_ID", CreateAccount = true };
+            PlayFabClientAPI.LoginWithCustomID(request, LoginSuccessHandler, LoginFailureHandler);
+
+        }
+
+        private void PlayFabHelloWorld()
+        {
+            var request = new ExecuteCloudScriptRequest()
+            {
+                FunctionName = "helloWorld",
+                FunctionParameter = new { inputValue = "YOUR NAME" },
+            };
+            PlayFabClientAPI.ExecuteCloudScript(request, result =>
+            {
+                var str = JsonWrapper.SerializeObject(result.FunctionResult);
+                var deserialized = JsonWrapper.DeserializeObject(str);
+                JsonObject jsonResult = (JsonObject)deserialized;
+                object messageValue;
+                jsonResult.TryGetValue("messageValue", out messageValue);
+                Debug.Log("PlayFab/ " + (string)messageValue);
+            }, error =>
+            {
+                Debug.LogError(error.ErrorMessage);
+            });
+        }
+
+        private void LoginSuccessHandler(LoginResult loginResult)
+        {
+            Debug.Log("PlayFab/Login");
+            PlayFabHelloWorld();
+        }
+
+        private void LoginFailureHandler(PlayFabError error)
+        {
+            Debug.LogError(error.ErrorMessage);
+        }
+    }
+}
